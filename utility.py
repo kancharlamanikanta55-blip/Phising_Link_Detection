477a7f7a38f3d6393cc2950565eb5e51264a91c3
Manikanta <kancharlamanikanta55@gmail.com>
2026-03-13 18:08:19 +0530

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,69 +1,37 @@
 import re
-import pandas as pd
-#Use of IP or not in domain
-df=pd.read_csv('malicious_phish.csv\malicious_phish.csv')
+from urllib.parse import urlparse
+from tld import get_tld
+
+# --- FEATURE EXTRACTION FUNCTIONS ---
+
 def having_ip_address(url):
     match = re.search(
         '(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.'
         '([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\/)|'  # IPv4
-        '((0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\/)' # IPv4 in hexadecimal
+        '((0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\/)' # IPv4 hex
         '(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}', url)  # Ipv6
-    if match:
-        # print match.group()
-        return 1
-    else:
-        # print 'No matching pattern found'
-        return 0
-df['use_of_ip'] = df['url'].apply(lambda i: having_ip_address(i))
-
-from urllib.parse import urlparse
+    return 1 if match else 0
 
 def abnormal_url(url):
     hostname = urlparse(url).hostname
     hostname = str(hostname)
     match = re.search(hostname, url)
-    if match:
-        # print match.group()
-        return 1
-    else:
-        # print 'No matching pattern found'
-        return 0
-
-
-df['abnormal_url'] = df['url'].apply(lambda i: abnormal_url(i))
+    return 1 if match else 0
 
 def count_dot(url):
-    count_dot = url.count('.')
-    return count_dot
-
-df['count.'] = df['url'].apply(lambda i: count_dot(i))
-df.head()
+    return url.count('.')
 
 def count_www(url):
-    url.count('www')
     return url.count('www')
 
-df['count-www'] = df['url'].apply(lambda i: count_www(i))
-
 def count_atrate(url):
-     
     return url.count('@')
 
-df['count@'] = df['url'].apply(lambda i: count_atrate(i))
-
-
 def no_of_dir(url):
-    urldir = urlparse(url).path
-    return urldir.count('/')
-
-df['count_dir'] = df['url'].apply(lambda i: no_of_dir(i))
+    return urlparse(url).path.count('/')
 
 def no_of_embed(url):
-    urldir = urlparse(url).path
-    return urldir.count('//')
-
-df['count_embed_domian'] = df['url'].apply(lambda i: no_of_embed(i))
-
+    return urlparse(url).path.count('//')
 
 def shortening_service(url):
     match = re.search('bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|'
@@ -73,112 +41,57 @@ def shortening_service(url):
                       'db\.tt|qr\.ae|adf\.ly|goo\.gl|bitly\.com|cur\.lv|tinyurl\.com|ow\.ly|bit\.ly|ity\.im|'
                       'q\.gs|is\.gd|po\.st|bc\.vc|twitthis\.com|u\.to|j\.mp|buzurl\.com|cutt\.us|u\.bb|yourls\.org|'
                       'x\.co|prettylinkpro\.com|scrnch\.me|filoops\.info|vzturl\.com|qr\.net|1url\.com|tweez\.me|v\.gd|'
-                      'tr\.im|link\.zip\.net',
-                      url)
-    if match:
-        return 1
-    else:
-        return 0
-    
-    
-df['short_url'] = df['url'].apply(lambda i: shortening_service(i))
+                      'tr\.im|link\.zip\.net', url)
+    return 1 if match else 0
 
 def count_https(url):
     return url.count('https')
 
-df['count-https'] = df['url'].apply(lambda i : count_https(i))
-
 def count_http(url):
     return url.count('http')
-
-df['count-http'] = df['url'].apply(lambda i : count_http(i))
 
 def count_per(url):
     return url.count('%')
 
-df['count%'] = df['url'].apply(lambda i : count_per(i))
-
 def count_ques(url):
     return url.count('?')
-
-df['count?'] = df['url'].apply(lambda i: count_ques(i))
 
 def count_hyphen(url):
     return url.count('-')
 
-df['count-'] = df['url'].apply(lambda i: count_hyphen(i))
-
 def count_equal(url):
     return url.count('=')
-
-df['count='] = df['url'].apply(lambda i: count_equal(i))
 
 def url_length(url):
     return len(str(url))
 
-
-#Length of URL
-df['url_length'] = df['url'].apply(lambda i: url_length(i))
-#Hostname Length
-
 def hostname_length(url):
     return len(urlparse(url).netloc)
 
-df['hostname_length'] = df['url'].apply(lambda i: hostname_length(i))
-
-df.head()
-
 def suspicious_words(url):
-    match = re.search('PayPal|login|signin|bank|account|update|free|lucky|service|bonus|ebayisapi|webscr',
-                      url)
-    if match:
-        return 1
-    else:
-        return 0
-df['sus_url'] = df['url'].apply(lambda i: suspicious_words(i))
-
+    match = re.search('PayPal|login|signin|bank|account|update|free|lucky|service|bonus|ebayisapi|webscr', url)
+    return 1 if match else 0
 
 def digit_count(url):
     digits = 0
     for i in url:
         if i.isnumeric():
-            digits = digits + 1
+            digits += 1
     return digits
-
-
-df['count-digits']= df['url'].apply(lambda i: digit_count(i))
-
 
 def letter_count(url):
     letters = 0
     for i in url:
         if i.isalpha():
-            letters = letters + 1
+            letters += 1
     return letters
 
-
-df['count-letters']= df['url'].apply(lambda i: letter_count(i))
-
-df.head()
-
-#Importing dependencies
-from urllib.parse import urlparse
-from tld import get_tld
-import os.path
-
-#First Directory Length
 def fd_length(url):
-    urlpath= urlparse(url).path
+    urlpath = urlparse(url).path
     try:
         return len(urlpath.split('/')[1])
     except:
         return 0
-
-df['fd_length'] = df['url'].apply(lambda i: fd_length(i))
-
-#Length of Top Level Domain
-df['tld'] = df['url'].apply(lambda i: get_tld(i,fail_silently=True))
-
 
 def tld_length(tld):
     try:
@@ -186,15 +99,10 @@ def tld_length(tld):
     except:
         return -1
 
-df['tld_length'] = df['tld'].apply(lambda i: tld_length(i))
-
-
-
+# --- MAIN INTERFACE FUNCTION ---
 
 def main(url):
-    
     status = []
-    
     status.append(having_ip_address(url))
     status.append(abnormal_url(url))
     status.append(count_dot(url))
@@ -202,28 +110,21 @@ def main(url):
     status.append(count_atrate(url))
     status.append(no_of_dir(url))
     status.append(no_of_embed(url))
-    
     status.append(shortening_service(url))
     status.append(count_https(url))
     status.append(count_http(url))
-    
     status.append(count_per(url))
     status.append(count_ques(url))
     status.append(count_hyphen(url))
     status.append(count_equal(url))
-    
     status.append(url_length(url))
     status.append(hostname_length(url))
     status.append(suspicious_words(url))
     status.append(digit_count(url))
     status.append(letter_count(url))
     status.append(fd_length(url))
-    tld = get_tld(url,fail_silently=True)
-      
-    status.append(tld_length(tld))
     
+    tld_val = get_tld(url, fail_silently=True)
+    status.append(tld_length(tld_val))
     
-    
-
     return status
-
